7dc690803e84569fc4f80536bd51bf22df4e3d84
Neal Audenaert <neal@idch.org>
2013-09-22 13:42:03 -0500

    Added method to write colorized CC image

--- org.dharts.dia/src/org/dharts/dia/seg/ConnectedComponent.java
+++ org.dharts.dia/src/org/dharts/dia/seg/ConnectedComponent.java
@@ -1,5 +1,6 @@
 package org.dharts.dia.seg;
 
+import java.awt.image.WritableRaster;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -8,6 +9,20 @@ import org.dharts.dia.SimpleBoundingBox;
 
 public class ConnectedComponent {
 
+	public static void write(ConnectedComponent cc, WritableRaster raster, int[] color)
+	{
+		int bands = color.length;
+		for (int[] point : cc.points)
+		{
+			int x = point[0];
+			int y = point[1];
+			for (int b = 0; b < bands; b++) {
+				raster.setSample(x, y, b, color[b]);
+			}
+		}
+	}
+
+	// TODO make this immutable
 	private final List<int[]> points = new ArrayList<>();
 	private volatile int xMin = -1;
 	private volatile int xMax = -1;
